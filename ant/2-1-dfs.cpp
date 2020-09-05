c391a335c986c32b511a95aee33ce58645777858
Makoto Kurihara <mkuri8m@gmail.com>
2020-09-06 03:19:51 +0900

    Add time measurement

--- ant/2-1-dfs.cpp
+++ ant/2-1-dfs.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 bool dfs(int i, int sum, std::vector<int> &a, int k) {
   if (i == a.size()) return sum == k;
@@ -16,9 +17,12 @@ int main() {
   std::vector<int> a{1, 2, 4, 7};
   int k = 15;
 
+  auto start = std::chrono::system_clock::now();
   if (dfs(0, 0, a, k)) {
     std::cout << "yes" << std::endl;
   } else {
     std::cout << "no" << std::endl;
   }
+  auto end = std::chrono::system_clock::now();
+  std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us" << std::endl;
 }
